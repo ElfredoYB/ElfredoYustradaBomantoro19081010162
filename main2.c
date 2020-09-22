2d2dfa52b599f2bbfe9a5e482d8369a78277c6ad
ElfredoYB <71373008+ElfredoYB@users.noreply.github.com>
2020-09-22 22:11:38 +0700

    Update main2.c

--- main2.c
+++ main2.c
@@ -5,11 +5,11 @@ int main()
     printf("\tBIODATA \n\n");
     printf("Nama : Elfredo Yustrada Bomantoro\n");
     printf("TTL	: Tuban 17 Maret 2001\n");
-	printf("jenis kelamin : Laki-Laki\n");
-	printf("alamat : Keputih permai 6.B/8\n");
-	printf("NPM : 19081010162\n");
-    printf("Jurusan	: Teknik Informatika\n");
-    printf("email	: elfredoyb1@gmail.com\n");
+    printf("jenis kelamin : Laki-Laki\n");
+    printf("alamat : Keputih permai 6.B/8\n");
+    printf("NPM : 19081010162\n");
+    printf("Jurusan : Teknik Informatika\n");
+    printf("email : elfredoyb1@gmail.com\n");
     
 return 0;
 }
