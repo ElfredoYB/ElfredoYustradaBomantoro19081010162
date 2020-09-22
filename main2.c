fc2250b86aac8d14d467ad6880fc4bc8dc58d7a0
ElfredoYB <elfredoyb1@gmail.com>
2020-09-22 22:05:05 +0700

    first commit

---
+++ main2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+
+    printf("\tBIODATA \n\n");
+    printf("Nama : Elfredo Yustrada Bomantoro\n");
+    printf("TTL	: Tuban 17 Maret 2001\n");
+	printf("jenis kelamin : Laki-Laki\n");
+	printf("alamat : Keputih permai 6.B/8\n");
+	printf("NPM : 19081010162\n");
+    printf("Jurusan	: Teknik Informatika\n");
+    printf("email	: elfredoyb1@gmail.com\n");
+    
+return 0;
+}
